ec0434c82729e0812ef7bbd463eca54b27283081
Khalyutin <khalyutin@notesoft.net>
2023-04-30 14:45:43 +0300

    -

--- public/items/implemenation/050000_frXXX_setup.cmake
+++ public/items/implemenation/050000_frXXX_setup.cmake
@@ -1,5 +1,8 @@
 cmake_policy(SET CMP0091 NEW)
 set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>")
-set(enRC_FLAGS " -c65001 -i ${enWXDIR}/native/include") 
+set(enRC_FLAGS      "-i ${enWX_ROOT_DIR}/include -c65001 ") 
+set(WXDIR              "${enWX_ROOT_DIR}")
+set(wxWidgets_ROOT_DIR "${enWX_ROOT_DIR}")
+set(wxWidgets_LIB_DIR  "${enWX_LIB_DIR}")
 
-
+# ./update-revision-info
